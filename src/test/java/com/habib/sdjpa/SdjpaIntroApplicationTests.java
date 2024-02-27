5dda4d7f0bfcf77b2d05ea1ff2eda98c076344b6
habibur-rahman-swe <habibur0199mhr@gmail.com>
2024-02-27 08:12:26 +0600

    Initial commete

---
+++ src/test/java/com/habib/sdjpa/SdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habib.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
